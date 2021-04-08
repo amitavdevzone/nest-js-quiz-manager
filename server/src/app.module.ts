e069aa6bdbba206c65fdbdeb5ab238ba6a1de753
Amitav Roy <reachme@amitavroy.com>
2021-04-08 22:03:19 +0530

    Configuration value was wrong

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -6,7 +7,11 @@ import { typeOrmConfigAsync } from './config/typeorm.config';
 import { QuizModule } from './modules/quiz/quiz.module';
 
 @Module({
-  imports: [QuizModule, TypeOrmModule.forRootAsync(typeOrmConfigAsync)],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync(typeOrmConfigAsync),
+    QuizModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
